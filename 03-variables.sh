ec86614c22a8589e7fe5bf398ee4eebf23b5256b
nishanth talari <talarinishanth@gmai.com>
2025-02-05 00:04:18 +0530

    thrid scirpt added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+PERSON1=Ramesh # no space before and after equal
+PERSON2=Suresh
 
-echo "Rahim:: Hi Robert"
-echo "Robert:: Hello Rahim"
-echo "Rahim:: How are you doing?"
-echo "Robert:: I am good. How are you?"
-
+echo "$PERSON1:: Hi $PERSON2"
+echo "${PERSON2}:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+read -s USERNAME 
+
+echo "username entered: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
